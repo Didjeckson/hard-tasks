bd18bb00cef10d483a81ed51b49de6a5cf996bba
Di-djeckson <didjeckson@gmail.com>
2020-09-20 15:40:57 +0300

    первый пункт

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let arr = ['727275', '132523', '25243', '25245475', '4353464', '2462462', '24436'];
+
+for (let i = 0; i < arr.length; i++) {
+    let arrElement = arr[i];
+    if (arrElement[0] === '2' || arrElement[0] === '4') {
+        console.log(arrElement);
+        continue;
+    }
+}
