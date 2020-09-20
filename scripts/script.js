470c2ec1b3d4a9a5dc7848ce0067bfd1b92fe0cc
Di-djeckson <didjeckson@gmail.com>
2020-09-20 15:52:18 +0300

    сделано

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,16 @@ for (let i = 0; i < arr.length; i++) {
         continue;
     }
 }
+
+for (let i = 1; i <= 100; i++) {
+    let key = 0;
+    for (let j = 1; j <= i; j++) {
+        if (i % j === 0) {
+            key++;
+        }
+    }
+    if (key === 2) {
+        console.log(i);
+    }
+    key = 0;
+}
