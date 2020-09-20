b47e42b25002d030db02473682d1a74642ebc40d
Di-djeckson <didjeckson@gmail.com>
2020-09-20 15:26:08 +0300

    сделано

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,24 @@
 
 function text(str) {
 
+    console.log(typeof (str));
+
     if (typeof (str) !== 'string') {
-        alert('it is not a string!');
-        text(str);
+        alert('it is not a string! Please reload a website. And change "n"');
+        // text(str);
+        return;
     }
 
     str.trim();
 
+    if (str.length > 30) {
+        str = str.substr(0, 30);
+        str = str + '...';
 
+    }
+    console.log(str);
 }
 
-let n = 4;
+let n = 'fhjkuty';
 
 text(n);
